9f109bd1eca24e5c66696f2d4357cbbc379b918e
Suy13 <uysamantha88@gmail.com>
2020-04-22 18:13:10 -0400

    apr 22 & 23

---
+++ MonthtoDays.py
@@ -0,0 +1,13 @@
+month = input()
+if month == 'February':
+    print ('28 or 29 days')
+elif month == 'April':
+    print ('30 days')
+elif month =='June':
+    print ('30 days')
+elif month == 'September':
+    print ('30 days')
+elif month == 'November':
+    print ('30 days')
+else:
+    print ('31 days')
